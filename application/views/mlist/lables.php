bc9991817e3c07769dcf3af835663bfb46d48dc3
root <root@mx1>
2020-05-21 15:50:55 +0530

    Labels output I. Added link on first page to go home.

--- application/views/mlist/lables.php
+++ application/views/mlist/lables.php
@@ -1,9 +1,9 @@
 <?php
 tfpdf();
-$pdf = new tFPDF('P','mm',array(210,297));
+$pdf = new tFPDF('L','mm',array(210,30));
 $lm=8;
 $font='Arial';
-$fs=7.25;
+$fs=14;
 $tm=15;
 $pdf->setTopMargin($tm);
 $pdf->setLeftMargin($lm);
@@ -11,39 +11,23 @@ $pdf->SetAutoPageBreak(false);
 $pdf->AddPage();
 $pdf->SetFont($font,'',$fs);
 $col=0;
+$pdf->SetFillColor(200);
+$pdf->cell(180,5,'Click here to go Home. Pl print page 2 onwards',0,1,'C',1,site_url('login/home'));
+$pdf->AddPage('P',array(210,297));
+$fs=7.25;
+$pdf->SetFont($font,'',$fs);
 foreach ($addresses as $data):
 
 if ($data['pin']==null||$data['pin']==0):
-$data['pin']='';
-endif;
-
-if ($data['city']==''):
-	$data['city']="PIN: ";
-else:
-	$data['city']=$data['city'].": ";
-endif;	
-/*
-if ($data['add1']==''):
-	$data['add1']=$data['add2'];
-	$data['add2']='';
-endif;
-
-if ($data['add2']==''):
-	$data['add2']=$data['add3'];
-	$data['add3']='';
-endif;
-
-if ($data['add3']==''):
-	$data['add3']=$data['add4'];
-	$data['add4']='';
-endif;
-
-if ($data['add4']==''):
-	$data['add4']=$data['city'].$data['pin'];
-	$data['city']='';
 	$data['pin']='';
+else:
+	if ($data['city']==''):
+		$data['city']="PIN: ";
+	else:
+		$data['city']=$data['city'].": ";
+	endif;	
 endif;
-*/
+
 $y=$pdf->getY();
 $pdf->Cell(60,4,$data['id'],0,1,'L');
 $pdf->Cell(60,4,$data['hon']. ' '.$data['name'],0,1,'L');
@@ -65,7 +49,7 @@ $col=0;
 $pdf->SetX($lm);
 $pdf->SetLeftMargin($lm);
 	if (297-$y<43):
-		$pdf->AddPage();
+		$pdf->AddPage('P',array(210,297));
 		$pdf->SetY(15);
 	else:
 		$pdf->SetY($y+9.5);
@@ -74,17 +58,7 @@ endif;
 endforeach;
 
 
-$filename=SAVEPATH.$place."_Labels_".count($addresses)."pdf";
-$pdf->Output($filename);
+$filename=$place."_Labels_".count($addresses)."pdf";
+$pdf->Output($filename,'I');
 
-
-
-
-
-/*
-echo "<pre>";
-print_r($blore);
-echo "</pre>";
-echo "<a href=".site_url('login/home').">Go Home</a href>";
-*/
 ?>
