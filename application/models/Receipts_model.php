3a39cd77747b393f55689d933be20416e01d08b2
root <root@mx1>
2020-07-01 11:41:26 +0530

    in daily cash report, changed where criteria from sub_series to payment_mode

--- application/models/Receipts_model.php
+++ application/models/Receipts_model.php
@@ -67,7 +67,7 @@ class receipts_model extends CI_Model{
 	$sql=$this->db->select_max('no', 'maxno');
 	$sql=$this->db->select_sum('amount', 'amt');
 	$sql=$this->db->where('date',$date);
-	$sql=$this->db->where('sub_series','Cash');
+	$sql=$this->db->where('mode_payment','Cash');
 	$sql=$this->db->get('receipts');
 	$result=$sql->row();
 	if($result->minno!==null):
